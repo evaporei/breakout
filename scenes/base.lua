c3505b72ed2af63a44d7886dbeca65955647484e
Eva Pace <eba.pachi@gmail.com>
2024-08-06 12:41:05 -0300

    render title (fn to method)

--- scenes/base.lua
+++ scenes/base.lua
@@ -1,9 +1,9 @@
 local BaseScene = {}
 
-function BaseScene:keypressed()
+function BaseScene:keypressed(_)
 end
 
-function BaseScene:update()
+function BaseScene:update(_)
 end
 
 function BaseScene:render()
--- scenes/start.lua
+++ scenes/start.lua
@@ -5,24 +5,6 @@ local fonts = require('fonts')
 local StartScene = {}
 setmetatable(StartScene, { __index = BaseScene })
 
-local function drawTitle(title, time)
-    local font = fonts.large
-    love.graphics.setFont(font)
-
-    local startWidth = (GAME_WIDTH - font:getWidth(title)) / 2
-    local chHeight = font:getHeight()
-    for i = 1, #title do
-        local color = RAINBOW[i]
-        love.graphics.setColor(color.r, color.g, color.b, 255)
-
-        local ch = title:sub(i, i)
-        local chWidth = font:getWidth(ch)
-        startWidth = startWidth + chWidth / 2
-        love.graphics.print(ch, startWidth, GAME_HEIGHT / 4 + math.sin(time * 5 + 20 * i) * 10, 0, 1, 1, chWidth / 2, chHeight / 2)
-        startWidth = startWidth + chWidth / 2
-    end
-end
-
 function StartScene.new(stateMachine)
     local self = {}
 
@@ -45,8 +27,26 @@ function StartScene:keypressed(key)
     end
 end
 
+function StartScene:renderTitle()
+    local font = fonts.large
+    love.graphics.setFont(font)
+
+    local startWidth = (GAME_WIDTH - font:getWidth(self.title)) / 2
+    local chHeight = font:getHeight()
+    for i = 1, #self.title do
+        local color = RAINBOW[i]
+        love.graphics.setColor(color.r, color.g, color.b, 255)
+
+        local ch = self.title:sub(i, i)
+        local chWidth = font:getWidth(ch)
+        startWidth = startWidth + chWidth / 2
+        love.graphics.print(ch, startWidth, GAME_HEIGHT / 4 + math.sin(self.time * 5 + 20 * i) * 10, 0, 1, 1, chWidth / 2, chHeight / 2)
+        startWidth = startWidth + chWidth / 2
+    end
+end
+
 function StartScene:render()
-    drawTitle(self.title, self.time)
+    self:renderTitle()
 end
 
 return StartScene
